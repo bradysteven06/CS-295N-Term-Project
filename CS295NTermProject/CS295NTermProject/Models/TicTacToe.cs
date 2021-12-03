9a6a3efee9002b07f90a633b6736826fe8d30d29
Steven Brady <bradys@my.lanecc.edu>
2021-12-03 09:41:59 -0800

    finished quiz

---
+++ CS295NTermProject/CS295NTermProject/Models/TicTacToe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS295NTermProject.Models
+{
+    public class TicTacToe
+    {
+        public string PlayerX = "X";
+        public string PlayerO = "O";
+    }
+}
